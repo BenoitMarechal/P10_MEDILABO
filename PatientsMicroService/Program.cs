d5db882b3fee5634ca026d8fb9c185aae356ea13
Benoit <bmarechal@vpauto.eu>
2025-08-21 15:19:21 +0200

    Gateway configured

--- PatientsMicroService/Program.cs
+++ PatientsMicroService/Program.cs
@@ -1,21 +1,17 @@
 using Microsoft.EntityFrameworkCore;
-
 using PatientMicroService.Data;
-
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Configure to listen on all interfaces
 builder.WebHost.UseUrls("http://0.0.0.0:80");
 
 var app = builder.Build();
@@ -27,19 +23,16 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+// REMOVE THIS LINE - it's causing issues with HTTP communication
+// app.UseHttpsRedirection();
 
 app.UseAuthorization();
-
 app.MapControllers();
+
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-    db.Database.EnsureCreated(); // or db.Database.Migrate() if you're using migrations
+    db.Database.EnsureCreated();
 }
 
 app.Run();
-
-
-
-
--- apiGateway/Program.cs
+++ apiGateway/Program.cs
@@ -6,7 +6,15 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 builder.Services.AddOcelot();
 
+// Add this line to force port 80
+builder.WebHost.UseUrls("http://0.0.0.0:80");
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
 
 await app.UseOcelot();
 app.Run();
